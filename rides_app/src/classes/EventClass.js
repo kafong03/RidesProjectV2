2880b8ab529da6d9cf4451ac97dc896f48e53a1d
Connor Lee <connojl2@uci.edu>
2024-07-13 17:13:45 -0700

    Initial Event Class

---
+++ rides_app/src/classes/EventClass.js
@@ -0,0 +1,14 @@
+class EventClass{
+    static get SUNDAY1ST () { return 0};
+    static get SUNDAY2ND () { return 1};
+    static get SUNDAY3RD () { return 2};
+    static get FRIDAY () { return 3};
+    static get OTHER () { return 4};
+    
+    constructor(id,  eventName, date, type){
+        this._id = id;
+        this.eventName = eventName;
+        this.date = date;
+        this.type = type;
+    }
+}
