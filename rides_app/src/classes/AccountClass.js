2fc8d7bd468ace510ef798739c86f2fb9f59d328
Connor Lee <connojl2@uci.edu>
2024-07-13 17:15:47 -0700

    Initial Account Class

---
+++ rides_app/src/classes/AccountClass.js
@@ -0,0 +1,9 @@
+class AccountClass{
+    constructor(id, name, accountId){
+        this._id = id;
+        this.auth0Name = name;
+        this.accountId = accountId;
+    }   
+}
+
+export default AccountClass;
