376a3a12b7cfbacd01f880a4a072458ffff8cc04
Connor Lee <connojl2@uci.edu>
2024-07-20 13:16:55 -0700

    Add default variables to Passenger and Driver classes

--- rides_app/src/classes/DriverClass.js
+++ rides_app/src/classes/DriverClass.js
@@ -3,7 +3,7 @@ class DriverClass{
     static get ASSIGNED () { return 1};
     static get ASSIGNING () { return 2};
 
-    constructor(id, name, address, friday, sunday1st, sunday2nd, sunday3rd, seats, contact){
+    constructor(id, name, address, friday, sunday1st, sunday2nd, sunday3rd, seats, contact, seatsLeft = new Map(), assigned = new Map(), unavailable = new Set(), note = "", cars = new Map(), labels = []) {
         this._id = id; //Mongo DB Id
         this.name = name; //String
         this.address = address; //String
@@ -12,13 +12,13 @@ class DriverClass{
         this.sunday2nd = sunday2nd; //Boolean
         this.sunday3rd = sunday3rd; //Boolean
         this.seats = seats; //Int
-        this.seatsLeft = new Map(); //Map<eventId, num seats left
-        this.assigned = new Map(); //Map<eventId, int> Use the UNASSIGNED/ASSIGNED/ASSIGNING above
-        this.unavailable = new Set(); //Set<date>
         this.contact = contact; //String
-        this.note = ""; //String
-        this.labels = []; //Array of String?
-        this.cars = new Map(); //Map<eventId, carId>
+        this.seatsLeft = seatsLeft; //Map<eventId, num seats left
+        this.assigned = assigned; //Map<eventId, int> Use the UNASSIGNED/ASSIGNED/ASSIGNING above
+        this.unavailable = unavailable; //Set<date>
+        this.note = note; //String
+        this.cars = cars; //Map<eventId, carId>
+        this.labels = labels; //Array of String?
     }
 }
 
--- rides_app/src/classes/PassengerClass.js
+++ rides_app/src/classes/PassengerClass.js
@@ -1,5 +1,5 @@
 class PassengerClass{
-    constructor(id, passengerName, location, address, friday, sunday1st, sunday2nd, sunday3rd, contact){
+    constructor(id, passengerName, location, address, friday, sunday1st, sunday2nd, sunday3rd, contact, assigned = new Map(), flagged = new Set(), notes = "", cars = new Map(), labels = []){
         this._id = id; //MongoDB Id
         this.name = passengerName; //String
         this.location = location; //String
@@ -8,12 +8,12 @@ class PassengerClass{
         this.sunday1st = sunday1st; //Boolean
         this.sunday2nd = sunday2nd; //Boolean
         this.sunday3rd = sunday3rd; //Boolean
-        this.assigned = new Map(); //Map<eventId, boolean>
-        this.flagged = new Set(); //Set<eventId>
         this.contact = contact; //String
-        this.labels = []; //Array of string
-        this.notes = ""; //String
-        this.cars = new Map(); //Map<eventId, carId>
+        this.assigned = assigned; //Map<eventId, boolean>
+        this.flagged = flagged; //Set<eventId>
+        this.notes = notes; //String
+        this.cars = cars; //Map<eventId, carId>
+        this.labels = labels; //Array of string
     }
 }
 
