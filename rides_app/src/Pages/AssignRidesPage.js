48ecdbc967dcb0417603412929a7ca98b3d6e288
Connor Lee <connojl2@uci.edu>
2024-09-23 14:30:56 -0700

    Basic test

--- rides_app/src/Pages/AssignRidesPage.js
+++ rides_app/src/Pages/AssignRidesPage.js
@@ -3,11 +3,15 @@ import { AgGridReact } from 'ag-grid-react';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 import { ModuleRegistry } from '@ag-grid-community/core';
 import PassengerClass from '../classes/PassengerClass';
+import DriverClass from '../classes/DriverClass';
 import "../CSS/AssignPage.css";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+var testDriverData1 = new DriverClass(2, "test1", "testAddress1", true, false, true, false, 3, "contact", new Map(), new Map(), new Set(), "note", new Map(), []);
+var testDriverData2 = new DriverClass(1, "test", "testAddress", true, false, true, false, 4, "contact", new Map(), new Map(), new Set(), "note", new Map(), []);
 
 var testPassengerData1 = new PassengerClass(1, "testPassenger", "testLocation", "testAddress", true, false, true, false,"contact",  new Map(), new Set(), "", new Map(), []);
 var testPassengerData2 = new PassengerClass(2, "testPassenger1", "testLocation", "testAddress1", true, false, true, false, "contact",  new Map(), new Set(), "", new Map(), []);
@@ -15,14 +19,14 @@ var testPassengerData3 = new PassengerClass(3, "testPassenger2", "testLocation1"
 var testPassengerData4 = new PassengerClass(4, "testPassenger3", "testLocation1", "testAddress2", true, false, true, false, "contact",  new Map(), new Set(), "", new Map(), []);
 
 const AssignRidesPage = () =>{
-    const [driverList, setDriverList] = useState([testPassengerData1, testPassengerData2,testPassengerData3, testPassengerData4]);
+    const [driverList, setDriverList] = useState([testDriverData1, testDriverData2]);
       
 
     const autoSizeStrategy = {
         type: 'fitCellContents'
     };
     const dataGridRef = useRef(null);
-    const [passengerList, setPassengerList] = React.useState([]);
+    const [passengerList, setPassengerList] = React.useState([testPassengerData1, testPassengerData2, testPassengerData3, testPassengerData4]);
     var dataGridApi = null;
 
     var gridApis = [];
@@ -48,34 +52,34 @@ const AssignRidesPage = () =>{
 
 
     const [dataRowData, setDataRowData] = useState(
-        // passengerList.map(passenger => ({
-        //     id: passenger.id,
-        //     name: passenger.name,
-        //     address: passenger.address,
-        //     location: passenger.location,
-        //     friday: passenger.friday ? "yes" : "no",
-        //     first: passenger.sunday1st ? "yes" : "no",
-        //     second: passenger.sunday2nd ? "yes" : "no",
-        //     third: passenger.sunday3rd ? "yes" : "no",
-        //     contact: passenger.contact,
-        //     flagged: passenger.flagged,
-        //     notes: passenger.notes
-        // }))
+        passengerList.map(passenger => ({
+            id: passenger._id,
+            name: passenger.name,
+            address: passenger.address,
+            location: passenger.location,
+            friday: passenger.friday ? "yes" : "no",
+            first: passenger.sunday1st ? "yes" : "no",
+            second: passenger.sunday2nd ? "yes" : "no",
+            third: passenger.sunday3rd ? "yes" : "no",
+            contact: passenger.contact,
+            flagged: passenger.flagged,
+            notes: passenger.notes
+        }))
 
  
-        [       {
-            id: "test",
-            name: "test name",
-            address: "test address",
-            location: "passenger.location",
-            friday: "no",
-            first: "yes",
-            second: "no",
-            third: "no",
-            contact: "passenger.contact",
-            flagged: "passenger.flagged",
-            notes: "passenger.notes"
-        }]
+        // [       {
+        //     id: "test",
+        //     name: "test name",
+        //     address: "test address",
+        //     location: "passenger.location",
+        //     friday: "no",
+        //     first: "yes",
+        //     second: "no",
+        //     third: "no",
+        //     contact: "passenger.contact",
+        //     flagged: "passenger.flagged",
+        //     notes: "passenger.notes"
+        // }]
     );
 
     const [testRowData, setTestRowData] = useState([{
@@ -117,7 +121,7 @@ const AssignRidesPage = () =>{
     >
         {driver.name}
     <AgGridReact 
-        rowData={testRowData}
+        rowData={[]}
         columnDefs={tempColDefs}
         autoSizeStrategy={autoSizeStrategy}
         rowDragManaged={true}
